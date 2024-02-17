e2381172a90dabe4972590e83adc1711e9747061
alvcodec <148978672+Alvicodes@users.noreply.github.com>
2024-02-17 22:46:05 +0000

    Initial commit

---
+++ Grocery-store/mysql/Products.sql
@@ -0,0 +1,17 @@
+SELECT * FROM grocery_store.products;
+SELECT * FROM grocery_store.uom;
+SELECT * FROM grocery_store.orders;
+SELECT * FROM grocery_store.orders_details;
+
+insert into products (name, uom_id, price_per_unit) values ('rice', 2, 4.2);
+insert into products (name, uom_id, price_per_unit) values ('bread', 1, 3.75);
+insert into products (name, uom_id, price_per_unit) values ('milk', 2, 3.75);
+insert into products (name, uom_id, price_per_unit) values ('eggs', 2, 1.75);
+insert into products (name, uom_id, price_per_unit) values ('toilet paper', 1, 2.5);
+insert into products (name, uom_id, price_per_unit) values ('shampoo', 1, 1.75);
+insert into products (name, uom_id, price_per_unit) values ('toothpaste', 2, 2.5);
+insert into products (name, uom_id, price_per_unit) values ('potatoes', 1, 1.75);
+insert into products (name, uom_id, price_per_unit) values ('soap', 1, 3.75);
+insert into products (name, uom_id, price_per_unit) values ('cereal', 1, 1.25);
+insert into products (name, uom_id, price_per_unit) values ('apple', 2, 1.25);
+insert into products (name, uom_id, price_per_unit) values ('melon', 2, 3.75);
